9527f00f1561026198727d7593efa715d1d1f2b9
amold <amold@LAPTOP-CML4U664>
2021-03-18 10:36:08 +0000

    Added first test case as well as implementing BetterBasket

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -2,6 +2,7 @@ package catalogue;
 
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Write a description of class BetterBasket here.
@@ -15,4 +16,30 @@ public class BetterBasket extends Basket implements Serializable
   
 
   // You need to add code here
+  @Override
+  public boolean add( Product pr )
+  { 
+	  if (!super.contains(pr)) {
+		  super.add(pr);
+
+		 } 
+	  else {
+		  for (int i=0; i < super.size(); i++) {
+			  if (super.get(i).equals(pr)) {
+				  double y = super.get(i).getPrice();
+				  super.get(i).setPrice(y + pr.getPrice());
+				  }      
+		  }
+	  }
+      Collections.sort(this, new Comparator<Product>() {
+    	  @Override
+    	  public int compare(Product p1, Product p2) {
+    		  return p1.getProductNum().compareTo(p2.getProductNum());
+    		  }
+    	  });
+      return true;
+  }
+  public String getDetails() {
+	  return "BetterBasket: \n" + super.getDetails();
+  }
 }
--- clients/Main.java
+++ clients/Main.java
@@ -2,6 +2,7 @@ package clients;
 import clients.backDoor.BackDoorController;
 import clients.backDoor.BackDoorModel;
 import clients.backDoor.BackDoorView;
+import clients.cashier.BetterCashierModel;
 import clients.cashier.CashierController;
 import clients.cashier.CashierModel;
 import clients.cashier.CashierView;
@@ -91,7 +92,7 @@ class Main
     window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
     Dimension pos = PosOnScrn.getPos();
     
-    CashierModel model      = new CashierModel(mlf);
+    BetterCashierModel model      = new BetterCashierModel(mlf);
     CashierView view        = new CashierView( window, mlf, pos.width, pos.height );
     CashierController cont  = new CashierController( model, view );
     view.setController( cont );
